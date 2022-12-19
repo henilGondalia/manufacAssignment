f9c947f81c2b41d59deea02637165887b7de0f8d
henil <henil97gondalia@gmail.com>
2022-12-19 18:06:37 +0530

    updated bar chart to show  the average of “Malic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ECharts from './component/Echarts';
 import { wineData } from './data/wineData';
 
@@ -6,7 +6,7 @@ function App() {
   const ScatterOption = {
     xAxis: {
       type: 'category',
-      data: wineData.map((data: any)=> data['Color intensity']),
+      data: wineData.map((data: any)=> Number(data['Color intensity'])),
       name: 'color intensity'
     },
     yAxis: {
@@ -23,12 +23,23 @@ function App() {
       trigger: 'axis',
     },
   };
+  // To find how many alcohol category is there
+  const AlcoholCategory = Array.from(new Set<any>(wineData.map((data: any)=> data['Alcohol'])))
+
+  // To get the avrage of every category's Malic Acid
+  let  MalicAcidAvg:number[] = [];
+  AlcoholCategory.forEach((category: number)=> {
+    const maicAcidArray = wineData.filter((data: any)=> category == data['Alcohol']).map((object: any)=> object['Malic Acid'])
+    const sum = maicAcidArray.reduce((a:number,b:number)=>a+b);
+    const avg = Number((sum/maicAcidArray.length).toFixed(2))
+    MalicAcidAvg.push(avg);
+  })
+  
   const BarOptions = {
     xAxis: {
       type: 'category',
-      data:  wineData.map((data: any)=> data['Alcohol']),
-      name: 'Alcohol',
-      axisLabel: {interval: 5}
+      data: AlcoholCategory,
+      name: 'Alcohol'
     },
     yAxis: {
       type: 'value',
@@ -36,7 +47,7 @@ function App() {
     },
     series: [
       {
-        data: wineData.map((data: any)=> data['Malic Acid']),
+        data: MalicAcidAvg,
         type: 'bar',
         
       },
